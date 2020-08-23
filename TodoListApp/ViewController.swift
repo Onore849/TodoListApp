13e4088145fab85477552635fe09d630bea17fc2
Onore849 <rion3376@gmail.com>
2020-08-23 14:47:12 +0900

    create tableViewcell

--- TodoListApp/ViewController.swift
+++ TodoListApp/ViewController.swift
@@ -12,24 +12,25 @@ class ViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     
+    var tasks = [String]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        // Get all current saved tasks
+        
     }
+    
+    
 
 
 }
 
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
+
+
+extension ViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
@@ -37,6 +38,25 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
+
     
 }
 
